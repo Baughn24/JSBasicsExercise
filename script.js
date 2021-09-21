8199423a1f00260d34ddca60ca2b2f18d5cecfa6
Allie Baughn <allison.baughn@gmail.com>
2021-09-20 19:17:43 -0500

    completed js exercise

---
+++ script.js
@@ -0,0 +1,49 @@
+// 1
+console.log("24");
+
+// 2
+// 1. Numbers 2. Strings 3. Booleans 4. Undefined 5. Null
+
+// 3
+// 1. Addition 2. Subtraction 3. Multiplication 4. Division 5. Exponents 6. Modulus
+
+// 4
+console.log(6%5);
+console.log(1%5);
+
+// 5
+console.log('I\'m Mary Poppins, Y\'all!');
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("Allie".length + "Baughn".length);
+
+// 8
+console.log("Avengers:Endgame"[15]);
+
+// BONUS 1
+console.log("\\");
+
+// BONUS 2
+console.log(5**4-9);
+
+// BONUS 3
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+
+// BONUS 4
+console.log("Cap"[1]);
+console.log("Vision"[0]);
+console.log("Okoye"[4]);
+console.log("Nick Fury"[0]);
+console.log("Groot"[0]);
+console.log("Dr.Strange"[9]);
+console.log("Thor"[3]);
+console.log("Star-Lord"[0]);
+
+// BONUS 5
+var m=25;
+var c=4;
+var u=m+c;
+console.log(u);
